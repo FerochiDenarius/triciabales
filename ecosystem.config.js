5173a8bfae829c03facd005d6f5acec8f1c8f291
KofiBright <ofosumenyabrightkofi@gmail.com>
2026-04-09 13:54:48 +0000

    role enforcement and super admin

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,17 @@
 module.exports = {
-    apps: [
-        {
-            name: "tricia-bales-api",
-            script: "java",
-            args: "-jar target/baleshop-0.0.1-SNAPSHOT.jar",
-            cwd: "/root/triciabales",
-            env: {
-                FRONTEND_URL: "https://www.yenkasa.xyz",
-                SMTP_HOST: "smtp.zoho.com",
-                SMTP_PORT: "587",
-                SMTP_SECURE: "false",
-                EMAIL_USER: "no.reply@yenkasa.xyz",
-                EMAIL_PASS: "@Yenkasa01@$$",
-                EMAIL_FROM: "Yenkasa Store <no.reply@yenkasa.xyz>"
-            }
+    apps: [{
+        name: "tricia-bales-api",
+        script: "java",
+        args: "-jar target/baleshop-0.0.1-SNAPSHOT.jar",
+        cwd: "/root/triciabales",
+        env: {
+            FRONTEND_URL: "https://www.yenkasa.xyz",
+            SMTP_HOST: "smtp.zoho.com",
+            SMTP_PORT: "587",
+            SMTP_SECURE: "false",
+            EMAIL_USER: "YOUR_EMAIL_USER",
+            EMAIL_PASS: "YOUR_EMAIL_PASSWORD",
+            EMAIL_FROM: "Yenkasa Store <YOUR_EMAIL>"
         }
-    ]
+    }]
 };
